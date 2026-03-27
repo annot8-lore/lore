bf83c3d3292d5f461e89500c3df9f90be60f2e43
Vidip G <vidipgg@gmail.com>
2026-03-26 22:07:05 -0500

    Fix integration tesT

--- tests/runTest.ts
+++ tests/runTest.ts
@@ -1,16 +1,25 @@
 import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
     const extensionTestsPath = path.resolve(__dirname, './integration/suite/index');
 
-    await runTests({
-        extensionDevelopmentPath,
-        extensionTestsPath,
-        // Disable other extensions for a clean test environment.
-        launchArgs: ['--disable-extensions', '--disable-gpu'],
-    });
+    // The extension returns early in activate() if no workspace folder is open,
+    // which means commands never get registered. Open a temp folder so activate()
+    // runs fully.
+    const tmpWorkspace = fs.mkdtempSync(path.join(os.tmpdir(), 'lore-ci-workspace-'));
+    try {
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: [tmpWorkspace, '--disable-extensions', '--disable-gpu'],
+        });
+    } finally {
+        fs.rmSync(tmpWorkspace, { recursive: true, force: true });
+    }
 }
 
 main().catch(err => {
